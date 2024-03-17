cd377517e4eabea17d721b0888cbd86c6153c3ec
Kanishka96ac <ac.kanishka@gmail.com>
2024-03-17 21:50:49 +0000

    Product interface created

---
+++ src/app/products/product.ts
@@ -0,0 +1,9 @@
+export interface IProducts {
+    productId : number,
+    productName : string,
+    productCode : string,
+    releaseDate : string,
+    starRating: number,
+    price: number,
+    imageUrl: string;
+}
